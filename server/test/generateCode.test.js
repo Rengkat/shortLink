2410b22f356fcd2d62d6cc4dfd42471e5f2b61b0
Rengkat <successinmotion11@.com>
2025-05-02 20:36:20 +0100

    generate code algorithm test implemented

---
+++ server/test/generateCode.test.js
@@ -0,0 +1,10 @@
+const generateCode = require("../utils/generateCode");
+describe("URL encoding Algorithm", () => {
+  const code = generateCode();
+  test("should generate a 6 character string", () => {
+    expect(code).toHaveLength(6);
+  });
+  test("should contain alphanumeric characters", () => {
+    expect(code).toMatch(/^[a-zA-Z0-9]+$/);
+  });
+});
